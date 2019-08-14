a840974ebcd2a49ce697e491e3ce4abfbfb294da
Mitesh Patel <miteshp@live.com>
2019-08-14 18:08:22 -0500

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'com.actioniq.play',
-    version = '0.2.6',
+    version = '0.2.8',
     description = 'Play plugin for Pants',
     url = 'https://github.com/ActionIQ-OSS/play-pants',
     author = 'Nadav Sr. Samet, Alex Moore, Nitay Joffe',
@@ -26,11 +26,15 @@ setup(
         'com',
         'com.actioniq',
         'com.actioniq.play',
+        'com.actioniq.play.targets',
+        'com.actioniq.play.tasks',
     ],
     packages = [
         'com',
         'com.actioniq',
         'com.actioniq.play',
+        'com.actioniq.play.targets',
+        'com.actioniq.play.tasks',
     ],
     install_requires = [
         'boto3==1.4.4',

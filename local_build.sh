548dc893431c44a3b1bb0656eaadcf764494d6e0
Mitesh Patel <miteshp@live.com>
2021-11-12 21:35:13 -0600

    Fix

--- local_build.sh
+++ local_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=0.0.10
+VERSION=0.0.11
 ./pants -ldebug publish.jar --local=~/.m2/repository --no-dryrun --override="com.thesamet#play-pants-tool=$VERSION" src/scala/playpants/tool:play-pants-tool
 aws s3 cp --recursive ~/.m2/repository/com/thesamet/play-pants-tool/$VERSION-SNAPSHOT/ s3://aiq-artifacts/releases/com/thesamet/play-pants-tool/$VERSION-SNAPSHOT/
